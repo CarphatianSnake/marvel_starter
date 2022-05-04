06b6f507e07a5ee8d29a5e034c8514a3bcefc929
Dmytro <sunsn4ke@gmail.com>
2022-05-04 13:01:57 +0300

    add PropTypes validation to CharList and CharInfo components

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
@@ -110,6 +111,10 @@ class CharList extends Component {
       </div>
     )
   }
+}
+
+CharList.propTypes = {
+  onCharSelected: PropTypes.func
 }
 
 export default CharList;
